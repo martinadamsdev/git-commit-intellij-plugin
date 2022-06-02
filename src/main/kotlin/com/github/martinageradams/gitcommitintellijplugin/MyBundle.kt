b2cd824ae55ec4186adfd9835c2db940bded4da9
GitHub Action <action@github.com>
2022-06-02 17:17:16 +0000

    Template cleanup

--- src/main/kotlin/com/github/martinageradams/gitcommitintellijplugin/MyBundle.kt
+++ src/main/kotlin/com/github/martinageradams/gitcommitintellijplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.martinageradams.gitcommitintellijplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/martinageradams/gitcommitintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.martinageradams.gitcommitintellijplugin.services
+
+import com.github.martinageradams.gitcommitintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
